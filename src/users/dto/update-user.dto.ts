413a1a0869428bd966cfcf1f6461fb7166669a0c
patelmeet01010 <patelmeet01010@gmail.com>
2024-08-16 17:00:39 +0530

    set password optional in update

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsOptional } from 'class-validator';
+import { IsEmpty, IsOptional } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsOptional()
+  @IsEmpty()
   password?: string;
 }
